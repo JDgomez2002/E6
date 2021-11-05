00b0dad9d6418182f11c4ab34370c59f17e562b6
JDgomez2002 <gom21429@uvg.edu.gt>
2021-11-05 09:03:10 -0600

    Inicio

---
+++ Vista.java
@@ -0,0 +1,24 @@
+//Universidad del Valle de Guatemala
+//Programación Orientada a Objetos
+//Catedrático Tomás Gálvez
+//Segundo Semestre 2021
+//Grupo 9:
+//José Daniel Gómez Cabrera
+//Carné 21429
+//Abner Iván García Alegría
+//21285
+//Sección 11
+//Actividad: Ejercicio 6
+
+import java.util.*;
+
+/**
+ * Clase Vista
+ * 
+ * @author Grupo 9
+ * @version Vista Class 1.1
+ */
+
+public class Vista {
+    
+}
