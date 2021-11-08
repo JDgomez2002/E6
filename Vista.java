a46a885886fec97ade91a4b20dd80e7ef281a656
GarciaAlegria <84537086+GarciaAlegria@users.noreply.github.com>
2021-11-08 15:24:49 -0600

    Vista modificada
    se añadieron algunos menús

--- Vista.java
+++ Vista.java
@@ -20,5 +20,150 @@ import java.util.*;
  */
 
 public class Vista {
+    private Scanner scan = new Scanner(System.in);
+    private Scanner stringscan = new Scanner(System.in);
     
+
+    public void Menuprincipal(){
+        boolean continuar = true;
+        while(continuar){
+        System.out.println("--------------------------------------------------------");
+        System.out.println("--------------------Bienvenidos------------------");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("");
+        System.out.println("¿Que acción desea ejecutar?");
+        System.out.println("1. Smartphones");
+        System.out.println("2. Teléfonos Celulares");
+        System.out.println("3. Teléfonos fijos");
+        System.out.println("4. Cámaras fotográficas");
+        System.out.println("5. Computador Personal (Desktop)");
+        System.out.println("6. Computador Personal (Laptop)");
+        System.out.println("7. Smart TV");
+        System.out.println("8. Tablets");
+        System.out.println("9. Smartwatch");
+        System.out.println("10. Salir");
+        }
+    }
+
+    public void MenuSmartphones(){
+        boolean continuar = true;
+        while(continuar){
+        System.out.println("--------------------------------------------------------");
+        System.out.println("--------------------Bienvenidos------------------");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("");
+        System.out.println("¿Que acción desea ejecutar?");
+        System.out.println("1. Hacer llamada");
+        System.out.println("2. Tomar Fotografía");
+        System.out.println("3. Navegar por internet");
+        System.out.println("4. Reproducir video");
+        System.out.println("5. Son portables");  
+}
+    }
+
+    public void MenuTelCelular(){
+        boolean continuar = true;
+        while(continuar){
+        System.out.println("--------------------------------------------------------");
+        System.out.println("--------------------Bienvenidos------------------");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("");
+        System.out.println("¿Que acción desea ejecutar?");
+        System.out.println("1. Hacer llamada");
+        System.out.println("2. Son portables");  
+    }
+}
+public void MenuTelFijo(){
+    boolean continuar = true;
+    while(continuar){
+    System.out.println("--------------------------------------------------------");
+    System.out.println("--------------------Bienvenidos------------------");
+    System.out.println("--------------------------------------------------------");
+    System.out.println("");
+    System.out.println("¿Que acción desea ejecutar?");
+    System.out.println("1. Hacer llamada");  
+}
+}
+public void MenuCamara(){
+    boolean continuar = true;
+    while(continuar){
+    System.out.println("--------------------------------------------------------");
+    System.out.println("--------------------Bienvenidos------------------");
+    System.out.println("--------------------------------------------------------");
+    System.out.println("");
+    System.out.println("¿Que acción desea ejecutar?");
+    System.out.println("1. Tomar Fotografía");
+    System.out.println("2. Reproducir video");
+    System.out.println("3. Son portables");  
+}
+}
+public void MenuCompuDesk(){
+    boolean continuar = true;
+    while(continuar){
+    System.out.println("--------------------------------------------------------");
+    System.out.println("--------------------Bienvenidos------------------");
+    System.out.println("--------------------------------------------------------");
+    System.out.println("");
+    System.out.println("¿Que acción desea ejecutar?");
+    System.out.println("1. Navegar por internet");
+    System.out.println("2. Reproducir video");
+    System.out.println("3. Ejecutar Videojuego");  
+}
+}
+public void MenuCompuLaptop(){
+    boolean continuar = true;
+    while(continuar){
+    System.out.println("--------------------------------------------------------");
+    System.out.println("--------------------Bienvenidos------------------");
+    System.out.println("--------------------------------------------------------");
+    System.out.println("");
+    System.out.println("¿Que acción desea ejecutar?");
+    System.out.println("1. Navegar por internet");
+    System.out.println("2. Ejecutar Videojuego");
+    System.out.println("3. Reproducir video");
+    System.out.println("4. Son portables");  
+}
+}
+public void MenuTV(){
+    boolean continuar = true;
+    while(continuar){
+    System.out.println("--------------------------------------------------------");
+    System.out.println("--------------------Bienvenidos------------------");
+    System.out.println("--------------------------------------------------------");
+    System.out.println("");
+    System.out.println("¿Que acción desea ejecutar?");
+    System.out.println("1. Navegar por internet");
+    System.out.println("2. Reproducir video");  
+}
+}
+
+public void MenuTablet(){
+    boolean continuar = true;
+    while(continuar){
+    System.out.println("--------------------------------------------------------");
+    System.out.println("--------------------Bienvenidos------------------");
+    System.out.println("--------------------------------------------------------");
+    System.out.println("");
+    System.out.println("¿Que acción desea ejecutar?");
+    System.out.println("1. Tomar Fotografía");
+    System.out.println("2. Navegar por internet");
+    System.out.println("3. Reproducir video");
+    System.out.println("4. Son portables");  
+}
+}
+public void MenuSmartwatch(){
+    boolean continuar = true;
+    while(continuar){
+    System.out.println("--------------------------------------------------------");
+    System.out.println("--------------------Bienvenidos------------------");
+    System.out.println("--------------------------------------------------------");
+    System.out.println("");
+    System.out.println("¿Que acción desea ejecutar?");
+    System.out.println("1. Tomar Fotografía");
+    System.out.println("2. Son portables");
+    System.out.println("3. Hacer llamadas");  
+}
+}
+
+
 }
