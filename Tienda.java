b12091874b85f845d63d76682a9e59e074eabaf2
JDgomez2002 <gom21429@uvg.edu.gt>
2021-11-09 15:14:15 -0600

    Tienda

--- Tienda.java
+++ Tienda.java
@@ -11,6 +11,7 @@
 //Actividad: Ejercicio 6
 
 import java.util.*;
+import java.io.*;
 
 /**
  * Clase Tienda
@@ -20,5 +21,71 @@ import java.util.*;
  */
 
 public class Tienda {
-    
+    private ArrayList<Producto> productos_disponibles = new ArrayList<Producto>();
+
+    public Tienda(){
+        leer_productos();
+
+    }
+
+    private void leer_productos(){
+        try{
+            String file = "productos.txt";
+            File myFile = new File(file);
+            Scanner scan = new Scanner(myFile);
+            while(scan.hasNextLine()){
+                Producto producto = null;
+                String linea = scan.nextLine();
+                String[] info = linea.split(",");
+                String id = info[0];
+                switch (id) {
+                    case "TEL1":
+                        producto = new SmartPhone(info);
+                        break;
+
+                    case "TEL2":
+                        producto = new Celular(info);
+                        break;
+
+                    case "TEL3":
+                        producto = new TelefonoFijo(info);
+                        break;
+
+                    case "CAM1":
+                        producto = new Camara(info);
+                        break;
+                        
+                    case "PC1":
+                        producto = new Desktop(info);
+                        break;
+
+                    case "PC2":
+                        producto = new Laptop(info);
+                        break;
+
+                    case "TV1":
+                        producto = new SmartTV(info);
+                        break;
+
+                    case "TB1":
+                        producto = new Tablet(info);
+                        break;
+                
+                    default://SW1
+                        producto = new SmartWatch(info);
+                        break;
+                }
+                this.productos_disponibles.add(producto);
+            }
+        }
+        catch(Exception e){
+            String s = "Tienda: leer_productos(): "+e.getMessage();
+            throw new RuntimeException(s);
+        }
+    }
+
+    public ArrayList<Producto> get_productos_disponibles(){
+        return this.productos_disponibles;
+    }
+
 }
